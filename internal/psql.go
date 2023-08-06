d3e6c74399e9c6ff520897835e545e91b1cc3c94
0xpeIpeI <hokumaru12@gmail.com>
2023-08-06 15:32:14 +0900

    move unused feat

--- internal/psql.go
+++ internal/psql.go
@@ -55,3 +55,25 @@ func switchInferType(clmType string) mifer.PrepareDataCallBack {
 		return mifer.NilPrepareDataCallBack
 	}
 }
+
+func TableNames(db *sql.DB, builder mifer.MiferBuilder, dbName string) ([]string, error) {
+	var tn = make([]string, 0)
+	rows, err := db.Query(`
+	SELECT table_name
+	FROM information_schema.tables
+	WHERE table_schema = 'public'
+	ORDER BY table_name;
+`)
+	if err != nil {
+		return nil, err
+	}
+	defer rows.Close()
+	for rows.Next() {
+		var clm string
+		if err := rows.Scan(&clm); err != nil {
+			return nil, err
+		}
+		tn = append(tn, clm)
+	}
+	return tn, nil
+}
