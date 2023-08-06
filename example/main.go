5de8201a99fbb827518c17c17254ffb7296cb0b5
0xpeIpeI <hokumaru12@gmail.com>
2023-08-06 18:04:24 +0900

    add example

---
+++ example/main.go
@@ -0,0 +1,36 @@
+package main
+
+import (
+	"context"
+	"database/sql"
+	"log"
+	"time"
+
+	"github.com/lll-lll-lll-lll/mifer"
+)
+
+func main() {
+	connStr := "postgres://mygo-postgres:mygo-postgres@localhost/mygo-postgresdb?sslmode=disable"
+	db, err := sql.Open("postgres", connStr)
+	if err != nil {
+		log.Fatal(err)
+	}
+	c := context.Background()
+	ctx, cancel := context.WithTimeout(c, time.Second*5)
+	defer cancel()
+	builder := mifer.NewPSQLBuilder(db)
+	clmns, err := builder.Scan(ctx, "users")
+	if err != nil {
+		log.Fatal(err)
+	}
+	randomData := mifer.DefaultMiferGenerator{}.Do(ctx, 100, mifer.DefaultUUIDPrepareDataCallBack)
+	idOpt := mifer.MiferOption{ColumnKey: "id", Datum: randomData}
+	queries, err := builder.BuildQueries(ctx, clmns, "users", idOpt)
+	if err != nil {
+		log.Fatal(err)
+	}
+	if err := mifer.Inject(ctx, db, queries); err != nil {
+		log.Fatal(err)
+	}
+
+}
